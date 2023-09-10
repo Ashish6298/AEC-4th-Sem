60533280f120eeebdae8b92d447f9c99b1f023a2
Ashish Goswami <116054951+Ashish6298@users.noreply.github.com>
2023-09-10 12:41:42 +0530

    Update diniingPhilosopher.go

--- diniingPhilosopher.go
+++ diniingPhilosopher.go
@@ -1,3 +1,4 @@
+
 package main
 import(
     "fmt"
@@ -5,54 +6,34 @@ import(
     "time"
 )
 
-type Chopstick struct{
-    sync.Mutex
-}
-
 func main(){
-    var numPhilosophers int
-    
-    fmt.Print("Enter no. of philosophers: ")
-    fmt.Scan(&numPhilosophers)
-    
     var wg sync.WaitGroup
-    chopsticks:=make([]Chopstick,numPhilosophers)
+    start:=make(chan struct{})
+    var num int
+    fmt.Printf("enter the no of workers")
+    fmt.Scan(&num)
     
-    for i:=0;i<numPhilosophers;i++{
+    for i:=1;i<=num;i++{
         wg.Add(1)
         go func(id int){
             defer wg.Done()
-            
-            fmt.Printf("Philosopher %d is thinking\n",id)
-            time.Sleep(time.Second)
-            left:=id
-            right:=(id+1)%numPhilosophers
-            
-            chopsticks[left].Lock()
-            chopsticks[right].Lock()
-            
-            fmt.Printf("Philosopher %d is eating\n",id)
+            <-start
+            fmt.Printf("Worker %d is starting to work\n",id)
             time.Sleep(time.Second)
             
-            fmt.Printf("Philosopher %d finished eating\n",id)
-            chopsticks[left].Unlock()
-            chopsticks[right].Unlock()
+            fmt.Printf("Worker %d has reached checkpoint\n",id)
+            time.Sleep(time.Second)
+            time.Sleep(time.Second)
+            
+            fmt.Printf("Worker %d resuming the work\n",id)
+            time.Sleep(time.Second)
+            
             
         }(i)
     }
+    
+    fmt.Println("Starting workers.....")
+    close(start)
     wg.Wait()
+    fmt.Println("All workers completed their work")
 }
-outpur
-// Enter no. of philosophers: 4
-// Philosopher 3 is thinking
-// Philosopher 0 is thinking
-// Philosopher 2 is thinking
-// Philosopher 1 is thinking
-// Philosopher 0 is eating
-// Philosopher 0 finished eating
-// Philosopher 3 is eating
-// Philosopher 3 finished eating
-// Philosopher 2 is eating
-// Philosopher 2 finished eating
-// Philosopher 1 is eating
-// Philosopher 1 finished eating
