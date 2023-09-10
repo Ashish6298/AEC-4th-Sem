3414fd60267d38c16c736ae55f7ec28694c67c12
Ashish Goswami <116054951+Ashish6298@users.noreply.github.com>
2023-09-10 12:57:42 +0530

    Update diniingPhilosopher.go

--- diniingPhilosopher.go
+++ diniingPhilosopher.go
@@ -1,39 +1,38 @@
-
 package main
-import(
-    "fmt"
-    "sync"
-    "time"
+
+import (
+	"fmt"
+	"sync"
+	"time"
 )
 
-func main(){
-    var wg sync.WaitGroup
-    start:=make(chan struct{})
-    var num int
-    fmt.Printf("enter the no of workers")
-    fmt.Scan(&num)
-    
-    for i:=1;i<=num;i++{
-        wg.Add(1)
-        go func(id int){
-            defer wg.Done()
-            <-start
-            fmt.Printf("Worker %d is starting to work\n",id)
-            time.Sleep(time.Second)
-            
-            fmt.Printf("Worker %d has reached checkpoint\n",id)
-            time.Sleep(time.Second)
-            time.Sleep(time.Second)
-            
-            fmt.Printf("Worker %d resuming the work\n",id)
-            time.Sleep(time.Second)
-            
-            
-        }(i)
-    }
-    
-    fmt.Println("Starting workers.....")
-    close(start)
-    wg.Wait()
-    fmt.Println("All workers completed their work")
+func main() {
+	var wg sync.WaitGroup
+	start := make(chan struct{})
+	var num int
+	fmt.Printf("Enter the number of pjilosopher")
+	fmt.Scan(&num)
+
+	for i := 1; i <= num; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			<-start
+
+			fmt.Printf("Philosopher %d is thinking\n", id)
+			time.Sleep(time.Second)
+
+			fmt.Printf("Philosopher %d is started to eat\n", id)
+			time.Sleep(time.Second)
+			time.Sleep(time.Second)
+
+			fmt.Printf("Philosopher %d is finished  eating\n", id)
+			time.Sleep(time.Second)
+		}(i)
+	}
+
+	fmt.Println(" Philosopher Starting  their tasks....")
+	close(start)
+	wg.Wait()
+	fmt.Println("All philosopher completed their task")
 }
