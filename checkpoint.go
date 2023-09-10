7792c80a26a600e3f511b16d4bbf9fd04d8e9c43
Ashish Goswami <116054951+Ashish6298@users.noreply.github.com>
2023-09-10 12:46:09 +0530

    Update checkpoint.go

--- checkpoint.go
+++ checkpoint.go
@@ -1,72 +1,39 @@
-package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
+package main
+import(
+    "fmt"
+    "sync"
+    "time"
 )
 
-func worker(id int, checkpoint chan bool, resume chan bool, wg *sync.WaitGroup) {
-	defer wg.Done()
-	fmt.Printf("Worker %d starting\n", id)
-	time.Sleep(time.Duration(id) * time.Second) // simulate work
-
-	fmt.Printf("Worker %d waiting for checkpoint\n", id)
-	checkpoint <- true // signal that checkpoint has been reached
-	<-resume           // wait for permission signal
-	fmt.Printf("Worker %d resuming\n", id)
-	// continue with the remaining work
+func main(){
+    var wg sync.WaitGroup
+    start:=make(chan struct{})
+    var num int
+    fmt.Printf("enter the no of workers")
+    fmt.Scan(&num)
+    
+    for i:=1;i<=num;i++{
+        wg.Add(1)
+        go func(id int){
+            defer wg.Done()
+            <-start
+            fmt.Printf("Worker %d is starting to work\n",id)
+            time.Sleep(time.Second)
+            
+            fmt.Printf("Worker %d has reached checkpoint\n",id)
+            time.Sleep(time.Second)
+            time.Sleep(time.Second)
+            
+            fmt.Printf("Worker %d resuming the work\n",id)
+            time.Sleep(time.Second)
+            
+            
+        }(i)
+    }
+    
+    fmt.Println("Starting workers.....")
+    close(start)
+    wg.Wait()
+    fmt.Println("All workers completed their work")
 }
-
-func main() {
-
-	numWorkers := 5
-	checkpoint := make(chan bool)
-	resume := make(chan bool)
-	var wg sync.WaitGroup
-
-	// launch the workers goroutines
-	for i := 0; i < numWorkers; i++ {
-		wg.Add(1)
-		go worker(i, checkpoint, resume, &wg)
-	}
-
-	// wait for all workers to reach the checkpoint
-	for i := 0; i < numWorkers; i++ {
-		<-checkpoint
-	}
-
-	fmt.Println("All workers reached the checkpoint")
-
-	fmt.Println("Resuming all workers")
-
-	// signal all workers to resume
-	for i := 0; i < numWorkers; i++ {
-		resume <- true
-	}
-	wg.Wait()
-	fmt.Println("All workers completed their work")
-}
-
-
-	/*
-		Output:
-		Worker 4 starting
-		Worker 0 starting
-		Worker 0 waiting for checkpoint
-		Worker 1 starting
-		Worker 2 starting
-		Worker 3 starting
-		Worker 1 waiting for checkpoint
-		Worker 2 waiting for checkpoint
-		Worker 3 waiting for checkpoint
-		Worker 4 waiting for checkpoint
-		All workers reached the checkpoint
-		Resuming all workers
-		Worker 1 resuming
-		Worker 4 resuming
-		Worker 2 resuming
-		Worker 3 resuming
-		Worker 0 resuming
-		All workers completed their work
-	*/
